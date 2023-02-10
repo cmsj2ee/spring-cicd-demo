39277c1499c098b07db70e4d1349e06f06dcc435
cmsj2ee <cmsj2ee@gmail.com>
2023-02-10 22:56:49 +0530

    created new spring git repo for cicd

---
+++ src/test/java/com/cicd/example/demo/SpringCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
